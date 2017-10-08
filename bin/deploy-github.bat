df9fd5e570cc0749ebf78cc8f153b41da16111d9
Aníbal Henrique <annibalhsouza@gmail.com>
2017-10-08 17:35:41 -0300

    changes files

--- bin/deploy-github.bat
+++ bin/deploy-github.bat
@@ -1,2 +1,2 @@
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+git checkout gh-pages && cd public && git add --all && git commit -m "Publishing to gh-pages" && cd .. && git checkout master
